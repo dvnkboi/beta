2dcb5df07b29326c301e841816fbc8f8a7c67f9a
dvnkboi <aymandb@outlook.com>
2021-01-27 22:57:13 +0100

    beta 0.2.9 bg update 2

--- src/components/background.vue
+++ src/components/background.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="fixed top-0 left-0 w-screen h-full overflow-y-hidden overflow-x-visible">
     <div :key="albumImg" class="absolute top-0 left-0 w-full h-full flex justify-center items-center z-0 transition duration-300">
-      <div class="absolute bgMask w-full h-full z-10"></div>
-      <div :style="{filter: `${imgSaturation}`}" class="absolute h-full w-full overflow-hidden z-0 flex justify-center items-start transition-all duration-100">
+      <div class="absolute bgMask w-full h-full z-20"></div>
+      <div :style="{ filter: `${saturation}` }" class="absolute h-full w-full transition-all duration-100 z-10 opacity-40">
+        <transition name="fade" appear class="z-50">
+          <div :key="key" v-show="show" :style="{ opacity: opacity, 'background-color': cssBackground }" class="background absolute w-full h-full transition-all duration-300 ease-linear bg-yellow-400"></div>
+        </transition>
+      </div>
+      <div :style="{ filter: `${imgSaturation}` }" class="absolute h-full w-full overflow-hidden z-0 flex justify-center items-start transition-all duration-100">
         <transition name="fade-up" appear>
-        <img :key="albumImg" :src="albumImg" alt="" class="bgImg w-full h-auto object-cover opacity-100"/>
+          <img :key="albumImg" :src="albumImg" alt="" class="bgImg w-full h-auto object-cover opacity-100" />
         </transition>
       </div>
     </div>
-    <transition name="fade" appear class="z-50">
-      <div :style="{filter: `${saturation}`}" class="h-full transition-all duration-100 z-50 opacity-40">
-        <div :key="key" v-show="show" :style="{ opacity: opacity, 'background-color': cssBackground }" class="background absolute w-full h-full transition-all duration-300 ease-linear bg-yellow-400"></div>
-      </div>
-    </transition>
   </div>
 </template>
 
@@ -31,32 +31,29 @@
       translation() {
         return `translateX(-${(1 - this.percent) * 100}%)`;
       },
-      opacity(){
-        if(this.percent>0.75){
-          return 1
-        }
-        else if(this.percent>0.5){
-          return 0.75
-        }
-        else if(this.percent>0.25){
-          return 0.5
-        }
-        else{
-          return 0.25
+      opacity() {
+        if (this.percent > 0.75) {
+          return 1;
+        } else if (this.percent > 0.5) {
+          return 0.75;
+        } else if (this.percent > 0.25) {
+          return 0.5;
+        } else {
+          return 0.25;
         }
       },
       cssBackground() {
         return this.currentColor;
       },
-      saturation(){
-        return `saturate(${(this.normalizedBassData * 150) + 50}%)`;
+      saturation() {
+        return `saturate(${this.normalizedBassData * 150 + 50}%)`;
       },
-      imgSaturation(){
-        return `saturate(${(this.normalizedBassData * 50) + 50}%)`;
+      imgSaturation() {
+        return `saturate(${this.normalizedBassData * 50 + 50}%)`;
       },
-      brightness(){
+      brightness() {
         return `saturate(${this.normalizedBassData * 100}%)`;
-      }
+      },
     },
     watch: {
       changed: function() {
@@ -72,7 +69,7 @@
       percent: Number,
       changed: Boolean,
       albumImg: String,
-      normalizedBassData: Number
+      normalizedBassData: Number,
     },
   };
 </script>

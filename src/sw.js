8b6d100304ae6b20ea012d0536a14a54547db5b4
dvnkboi <aymandb@outlook.com>
2021-01-28 05:47:16 +0100

    beta 0.3.0 PWA try 15

--- src/sw.js
+++ src/sw.js
@@ -4,40 +4,40 @@
 // import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
 // import { cacheNames } from 'workbox-core';
 
-importScripts('/WorkBox/workbox-routing/index.d.ts','/WorkBox/workbox-expiration/index.d.ts','/WorkBox/workbox-strategies/index.d.ts','/WorkBox/workbox-core/index.d.ts')
+// importScripts('/WorkBox/workbox-routing/index.d.ts','/WorkBox/workbox-expiration/index.d.ts','/WorkBox/workbox-strategies/index.d.ts','/WorkBox/workbox-core/index.d.ts')
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+suppressWarnings();
+precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new strategies.CacheFirst({
-        cacheName: 'images',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 60,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-            }),
-            new CacheableResponsePlugin({statuses: [0,200]}),
-        ],
-    })
-);
+// workbox.routing.registerRoute(
+//     ({ request }) => request.destination === 'image',
+//     new CacheFirst({
+//         cacheName: 'images',
+//         plugins: [
+//             new ExpirationPlugin({
+//                 maxEntries: 60,
+//                 maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//             }),
+//             new CacheableResponsePlugin({statuses: [0,200]}),
+//         ],
+//     })
+// );
 
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'script' ||
-        request.destination === 'style',
-    new strategies.StaleWhileRevalidate({
-        cacheName: 'static-resources',
-    })
-);
+// workbox.routing.registerRoute(
+//     ({ request }) => request.destination === 'script' ||
+//         request.destination === 'style',
+//     new StaleWhileRevalidate({
+//         cacheName: 'static-resources',
+//     })
+// );
 
 self.addEventListener("message", (e) => {
     if (e.data.action == 'skipWaiting') self.skipWaiting()
 });
 
-self.addEventListener('install', (event) => {
-  const urls = ['/'];
-  const cacheName = cacheNames.runtime;
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
-});
+// self.addEventListener('install', (event) => {
+//   const urls = ['/'];
+//   const cacheName = cacheNames.runtime;
+//   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
+// });

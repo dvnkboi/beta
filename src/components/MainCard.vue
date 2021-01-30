3687947245d253fd3a8f2667df0fb8ce8554f5cc
dvnkboi <aymandb@outlook.com>
2021-01-30 18:48:58 +0100

    beta 0.3.1 sizeable update

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="mainCard bg-black-dark bg-opacity-70 max-h-full flex w-full xl:w-2/5 xl:h-full flex-col min-h-120 justify-start items-start shadow-2xl pt-4 flex-none transition duration-300 overflow-auto">
+  <div class="mainCard bg-black-dark bg-opacity-70 max-h-full flex w-full min-h-screen sm:min-h-0 xl:w-2/5 xl:h-full flex-col min-h-120 justify-start items-start shadow-2xl pt-4 flex-none transition duration-300 overflow-auto">
     <div class="flex flex-col justify-center items-center md:justify-start md:items-start mt-4 flex-auto w-full"> 
-      <div :class="{ 'cursor-pointer': wikiAvailable, 'pointer-events-none': !wikiAvailable }" @click="showWiki = true" class="artistImgCont transform-gpu h-80 w-80 sm:w-96 sm:h-96 relative mx-8 transition-transform duration-300 overflow-hidden">
+      <div :class="{ 'cursor-pointer': wikiAvailable, 'pointer-events-none': !wikiAvailable }" @click="showWiki = true" class="artistImgCont transform-gpu h-64 w-64 xxs:h-80 xxs:w-80 sm:w-96 sm:h-96 relative mx-8 transition-transform duration-300 overflow-hidden">
         <transition name="fade-up" mode="out-in" appear>
           <img :key="'mainCover' + updatedCover" ref="coverArt" v-show="hasLoaded" @load="loaded" @error="updatedCover = aurLogo" v-loadedifcomplete :src="updatedCover" class="z-10 artistImg h-full w-full object-cover ring-2 ring-purple-100 ring-opacity-20 transition duration-300 absolute" alt="" />
         </transition>
@@ -11,8 +11,8 @@
       </div>
       <transition name="fade" mode="out-in" appear>
         <div v-if="wikiAvailable && showWiki" class="fixed overflow-x-hidden overflow-y-auto w-screen h-screen top-0 bottom-0 left-0 right-0 backdrop-blur bg-black-dark bg-opacity-80 z-50 pt-8 transition duration-300">
-          <div class="float-left flex justify-center items-center md:justify-start md:items-start flex-auto w-full h-80 sm:h-96">
-            <div @click="showWiki = false" class="wikiImgCont cursor-pointer transform-gpu hover:-translate-y-2 h-80 w-80 flex-shrink-0 sm:w-96 sm:h-96 relative mx-8 transition-transform duration-300 overflow-hidden">
+          <div class="float-left flex justify-center items-center md:justify-start md:items-start flex-auto w-full h-64 xxs:h-80 sm:h-96">
+            <div @click="showWiki = false" class="wikiImgCont cursor-pointer transform-gpu hover:-translate-y-2 h-64 w-64 xxs:h-80 xxs:w-80 sm:w-96 sm:h-96 flex-shrink-0 relative mx-8 transition-transform duration-300 overflow-hidden">
               <transition name="fade-up" mode="out-in">
                 <img :key="'mainCover' + updatedCover" ref="coverArt" v-show="hasLoaded" @load="loaded" @error="updatedCover = aurLogo" v-loadedifcomplete :src="updatedCover" class="z-10 artistImg h-full w-full object-cover ring-2 ring-purple-100 ring-opacity-20 transition duration-300 absolute" alt="" />
               </transition>
--- src/components/background.vue
+++ src/components/background.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="fixed top-0 left-0 w-screen h-full overflow-y-hidden overflow-x-visible">
     <div class="absolute top-0 left-0 w-full h-full flex justify-center items-center z-0 transition duration-300">
-      <div ref="scrollRadGr" style="opacity:0.4" class="absolute bgMaskRad w-full h-full z-20"></div>
-      <div ref="scrollLinGr" class="absolute bgMask w-full h-full z-40 transform origin-bottom scale-y-35"></div>
-      <div ref="scrollMask" style="opacity:1" class="absolute h-full w-full">
+      <div ref="scrollRadGr" style="opacity:0.4" class="absolute bgMaskRad w-full h-full z-20 transition duration-100"></div>
+      <div ref="scrollLinGr" class="absolute bgMask w-full h-full z-40 transform origin-bottom scale-y-35 transition duration-100"></div>
+      <div ref="scrollMask" style="opacity:1" class="absolute h-full w-full transition duration-100">
         <div :style="{ filter: `${saturation}` }" class="absolute h-full w-full transition-all duration-100 z-10 opacity-40">
           <transition name="fade" appear class="z-50">
             <div :key="key" v-show="show" :style="{ 'background-color': cssBackground }" class="background absolute w-full h-full transition-all duration-300 ease-linear bg-yellow-400"></div>
@@ -11,7 +11,7 @@
         </div>
         <div :style="{ filter: `${imgSaturation}` }" class="absolute h-full w-full overflow-hidden z-0 flex justify-center items-start transition-all duration-100">
           <transition name="fade-up" appear>
-            <img @load="grabColor()" @error="updatedCover = aurLogo" :key="updatedCover" :src="updatedCover" alt="" class="bgImg h-screen w-auto lg:w-full lg:h-auto object-cover opacity-100 transform scale-105" />
+            <img @load="grabColor()" @error="updatedCover = aurLogo" :key="updatedCover" :src="updatedCover" alt="" class="bgImg h-screen w-auto lg:w-full lg:h-auto object-cover opacity-100 transform scale-105 origin-top" />
           </transition>
         </div>
       </div>

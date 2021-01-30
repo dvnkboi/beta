7c58c24e9758ec5a1dcb39b37a457065414c8c42
dvnkboi <aymandb@outlook.com>
2021-01-30 18:58:18 +0100

    beta 0.3.1 loading...

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="fade-up" appear>
-    <div v-show="show" class="fixed pointer-events-none w-full h-36 bottom-5 transition duration-300">
-      <div class="loaderCont m-auto pointer-events-auto transform-gpu hover:-translate-y-2 h-36 w-36 bg-black-light shadow-xl rounded-4xl flex justify-center self-center items-center transition duration-300">
+    <div v-show="dShow" class="fixed pointer-events-none w-full h-36 bottom-5 transition duration-300">
+      <div @click="dShow = false" class="loaderCont click cursor-pointer m-auto pointer-events-auto transform-gpu hover:-translate-y-2 h-36 w-36 bg-black-light shadow-xl rounded-4xl flex justify-center self-center items-center transition duration-300">
         <RotateSquare5 background="rgb(209, 213, 219)" color="rgb(209, 213, 219)" size="rem"></RotateSquare5>
       </div>
     </div>
@@ -13,8 +13,18 @@
 import RotateSquare5 from './RotateSquare5.vue'
   export default {
     name: 'Loading',
+    data(){
+      return {
+        dShow:false
+      }
+    },
     props:{
         show: Boolean,
+    },
+    watch:{
+      show: function(){
+        this.dShow = this.show;
+      }
     },
     components: {
       RotateSquare5

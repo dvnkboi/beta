583e6ea29791e9e1a279cc805e07a209f519914a
dvnkboi <aymandb@outlook.com>
2021-02-05 21:16:33 +0100

    beta 0.3.7 ad integration

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -109,7 +109,7 @@
       importAudio() {
         let proxy = this;
         if (!this.audio) {
-          this.audio = new Silence('https://api.ampupradio.com:8443/TOP40.mp3?nocache=' + Date.now(), {
+          this.audio = new Silence('https://play.adtonos.com/8102/ampupradio', {
             volume: parseFloat(localStorage.getItem('volume')) || 1,
           });
 
